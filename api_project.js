e277469e1ecd2e4f7f5ae8fbbe400d4b6709eba5
Armando Barbosa <armandobs14@gmail.com>
2019-12-04 08:56:07 -0300

    Atualizando link para documentaçao da API

--- api_project.js
+++ api_project.js
@@ -6,7 +6,7 @@ define({
   "url": "http://api.sefaz.al.gov.br",
   "header": {
     "title": "Projeto Postman",
-    "content": "<ul>\n<li>Postman - Extensão para o chrome <a href=\"https://chrome.google.com/webstore/detail/postman/fhbjgbiflinjbdggehcddcbncdddomop\">dowload</a></li>\n<li>Projeto Postman base para o Hackathon <a href=\"https://github.com/SefazAL/Postman/blob/master/postman.zip?raw=true\">dowload</a></li>\n<li><a href=\"http://tsdn.tecnospeed.com.br/blog-da-consultoria-tecnica-tecnospeed/post/como-importar-requisicoes-http-para-o-postman/p/6126\">Como importar um projeto no postman</a></li>\n</ul>\n"
+    "content": "<ul>\n<li>Postman - Extensão para o chrome <a href=\"https://chrome.google.com/webstore/detail/postman/fhbjgbiflinjbdggehcddcbncdddomop\">dowload</a></li>\n<li>Projeto Postman base Oficial<a href=\"https://raw.githubusercontent.com/SefazAL/Postman/master/API.json\">dowload</a></li>\n<li><a href=\"http://tsdn.tecnospeed.com.br/blog-da-consultoria-tecnica-tecnospeed/post/como-importar-requisicoes-http-para-o-postman/p/6126\">Como importar um projeto no postman</a></li>\n</ul>\n"
   },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
